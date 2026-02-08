4778bb403c98bc3b300f0ed2e2a1786918b31a37
robertpendergrast <rlp2153@columbia.edu>
2026-02-08 16:25:01 -0500

    Testing Linter

---
+++ test/bad-code.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+
+int main (int argc, char** argv) {
+	 printf("Hello world!"    );
+	
+// this is bad code formatting
+// 
+
+/*
+
+*/
+
+for (int i = 0; i < 10; i++){
+if (i < 5) 
+{
+printf("%d",i);
+}
+}
+				return 0;
+}
+
+
+
+
+
+
