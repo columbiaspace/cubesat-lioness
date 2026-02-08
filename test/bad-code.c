e328e5149b9d215d4e755e6dfd31d5f7f5b16d0c
robertpendergrast <rlp2153@columbia.edu>
2026-02-08 16:30:41 -0500

    Linted

--- test/bad-code.c
+++ test/bad-code.c
@@ -1,26 +1,19 @@
 #include "stdio.h"
 
-int main (int argc, char** argv) {
-	 printf("Hello world!"    );
-	
-// this is bad code formatting
-// 
+int main(int argc, char **argv) {
+  printf("Hello world!");
 
-/*
+  // this is bad code formatting
+  //
 
-*/
+  /*
 
-for (int i = 0; i < 10; i++){
-if (i < 5) 
-{
-printf("%d",i);
+  */
+
+  for (int i = 0; i < 10; i++) {
+    if (i < 5) {
+      printf("%d", i);
+    }
+  }
+  return 0;
 }
-}
-				return 0;
-}
-
-
-
-
-
-
