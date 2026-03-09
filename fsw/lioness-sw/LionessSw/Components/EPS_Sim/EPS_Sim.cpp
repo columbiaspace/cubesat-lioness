1c93f7f31e76b0ee038f40a00cf8e314fdfb1ca6
Cedric <cedricliu07@gmail.com>
2026-03-08 20:54:16 -0400

    Made EPS Component

---
+++ fsw/lioness-sw/LionessSw/Components/EPS_Sim/EPS_Sim.cpp
@@ -0,0 +1,54 @@
+// ======================================================================
+// \title  EPS_Sim.cpp
+// \author xiwei
+// \brief  cpp file for EPS_Sim component implementation class
+// ======================================================================
+
+#include "LionessSw/Components/EPS_Sim/EPS_Sim.hpp"
+
+namespace Components {
+
+// ----------------------------------------------------------------------
+// Component construction and destruction
+// ----------------------------------------------------------------------
+
+EPS_Sim ::EPS_Sim(const char* const compName, F32 delta) : EPS_SimComponentBase(compName) {
+    this->_delta = delta;
+
+}
+
+EPS_Sim ::~EPS_Sim() {}
+
+// ----------------------------------------------------------------------
+// Handler implementations for typed input ports
+// ----------------------------------------------------------------------
+
+void EPS_Sim ::getBattery_handler(FwIndexType portNum, F32 &batteryLevel) {
+    batteryLevel = _batteryLevel;
+}
+
+void EPS_Sim ::run_handler(FwIndexType portNum, U32 context) {
+    if (_opMode)
+    {
+        _batteryLevel += _opMode * _delta;
+
+        if (_batteryLevel < 0) _batteryLevel = 0;
+        else if (_batteryLevel > 100) _batteryLevel = 100;
+    }
+}
+
+// ----------------------------------------------------------------------
+// Handler implementations for commands
+// ----------------------------------------------------------------------
+
+void EPS_Sim ::SET_EPS_SIM_OPMODE_cmdHandler(FwOpcodeType opCode, U32 cmdSeq, Components::OpMode opMode) {
+    _opMode = opMode;
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+}
+
+void EPS_Sim ::SET_EPS_SIM_BATTERY_LEVEL_cmdHandler(FwOpcodeType opCode, U32 cmdSeq, F32 batteryLevel) {
+    _batteryLevel = batteryLevel;
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+}
+
+}  // namespace Components
---
+++ fsw/lioness-sw/LionessSw/Components/EPS_Sim/EPS_Sim.fpp
@@ -0,0 +1,50 @@
+module Components {
+    port BatteryLevelGet(ref batteryLevel: F32)
+
+    enum OpMode {
+      DRAINING = -1,
+      OFF = 0,
+      CHARGING = 1
+    }
+}
+
+module Components {
+    
+    @ Basic simulator for EPS to emulate satellite power
+    passive component EPS_Sim {
+        sync input port run: Svc.Sched
+
+        @ Command for starting simulation
+        sync command SET_EPS_SIM_OPMODE(opMode: OpMode)
+
+        @ Command for changing the Simulation Mode
+        sync command SET_EPS_SIM_BATTERY_LEVEL(batteryLevel: F32)
+
+        event EPS_SimSetOpMode(opMode: OpMode) \
+            severity activity high \
+            format "EPS Simulation Operation Mode set to {}"
+
+        sync input port getBattery: BatteryLevelGet
+
+        ###############################################################################
+        # Standard AC Ports: Required for Channels, Events, Commands, and Parameters  #
+        ###############################################################################
+        @ Port for requesting the current time
+        time get port timeCaller
+
+        @ Enables command handling
+        import Fw.Command
+
+        @ Enables event handling
+        import Fw.Event
+
+        @ Enables telemetry channels handling
+        import Fw.Channel
+
+        @ Port to return the value of a parameter
+        param get port prmGetOut
+
+        @Port to set the value of a parameter
+        param set port prmSetOut
+    }
+}
