0021e5513840142f110c19582ed6c9a4f82728d3
Thomas Kourakos <thomaskourakos@Thomass-MacBook-Pro-14.local>
2026-03-27 18:46:03 -0400

    Updated calculateSunVector formula

--- fsw/lioness-sw/LionessSw/Components/SunSensor/SunSensor.cpp
+++ fsw/lioness-sw/LionessSw/Components/SunSensor/SunSensor.cpp
@@ -26,25 +26,25 @@ namespace Components {
     static U32 tick_counter = 0;
     tick_counter++;
 
+    // array values
+    m_diode_readings[0] = 10.0f; // PX
+    m_diode_readings[1] = 0.0f; // MX
+    m_diode_readings[2] = 2.0f; // PY
+    m_diode_readings[3] = 0.0f; // MY
+    m_diode_readings[4] = 0.0f; // PZ
+    m_diode_readings[5] = 0.0f; // MZ
+
+    m_diode_readings[0] = m_diode_readings[0] + ((F32)tick_counter)/10;
+
+    // Diode telemetry to see raw values in GDS
+    this->tlmWrite_DIODE_PX(m_diode_readings[0]);
+    this->tlmWrite_DIODE_MX(m_diode_readings[1]);
+    this->tlmWrite_DIODE_PY(m_diode_readings[2]);
+    this->tlmWrite_DIODE_MY(m_diode_readings[3]);
+    this->tlmWrite_DIODE_PZ(m_diode_readings[4]);
+    this->tlmWrite_DIODE_MZ(m_diode_readings[5]);
+
     if (tick_counter >= 5) {
-      // array values
-      m_diode_readings[0] = 1.0f; // PX
-      m_diode_readings[1] = 0.0f; // MX
-      m_diode_readings[2] = 0.5f; // PY
-      m_diode_readings[3] = 0.0f; // MY
-      m_diode_readings[4] = 0.0f; // PZ
-      m_diode_readings[5] = 0.0f; // MZ
-      
-
-      // Diode telemetry to see raw values in GDS
-      this->tlmWrite_DIODE_PX(m_diode_readings[0]);
-      this->tlmWrite_DIODE_MX(m_diode_readings[1]);
-      this->tlmWrite_DIODE_PY(m_diode_readings[2]);
-      this->tlmWrite_DIODE_MY(m_diode_readings[3]);
-      this->tlmWrite_DIODE_PZ(m_diode_readings[4]);
-      this->tlmWrite_DIODE_MZ(m_diode_readings[5]);
-
-
       this->log_ACTIVITY_LO_SunSensorTick();
       this->calculateSunVector();
       tick_counter = 0;  // reset 
@@ -74,6 +74,7 @@ namespace Components {
     F32 sum_x = m_diode_readings[0] + m_diode_readings[1];
     F32 sum_y = m_diode_readings[2] + m_diode_readings[3];
     F32 sum_z = m_diode_readings[4] + m_diode_readings[5];
+
     
     // calculate differences (Numerator)
     F32 diff_x = m_diode_readings[0] - m_diode_readings[1];
@@ -81,19 +82,19 @@ namespace Components {
     F32 diff_z = m_diode_readings[4] - m_diode_readings[5];
 
     // Calculate Sun Vectors. If s < epsilon, set to 0
-    F32 sx = (sum_x > epsilon) ? (diff_x / sum_x) : 0.0f;
-    F32 sy = (sum_y > epsilon) ? (diff_y / sum_y) : 0.0f;
-    F32 sz = (sum_z > epsilon) ? (diff_z / sum_z) : 0.0f;
+    F32 sx; // = (sum_x > epsilon) ? (diff_x / sum_x) : 0.0f;
+    F32 sy; // = (sum_y > epsilon) ? (diff_y / sum_y) : 0.0f;
+    F32 sz; // = (sum_z > epsilon) ? (diff_z / sum_z) : 0.0f;
 
     // Calculate length of vector
-    F32 magnitude_squared = (sx * sx) + (sy * sy) + (sz * sz);
+    F32 magnitude_squared = (diff_x * diff_x) + (diff_y * diff_y) + (diff_z * diff_z);
 
     // Scale to Unit Vector (magnitude = 1.0, direction only)
     if (magnitude_squared > epsilon) {
         F32 magnitude = sqrtf(magnitude_squared);
-        sx = sx / magnitude;
-        sy = sy / magnitude;
-        sz = sz / magnitude;
+        sx = diff_x / magnitude;
+        sy = diff_y / magnitude;
+        sz = diff_z / magnitude;
     } else {
         // clear the vector
         sx = 0.0f;
