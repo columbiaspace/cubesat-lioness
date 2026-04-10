041860a6562c6fd2a3a84b0f31b9efc51fd1d52d
Cedric <cedricliu07@gmail.com>
2026-04-10 19:28:16 -0400

    Added documentation and Initial values

--- fsw/lioness-sw/LionessSw/Components/EPS_Sim/EPS_Sim.cpp
+++ fsw/lioness-sw/LionessSw/Components/EPS_Sim/EPS_Sim.cpp
@@ -14,6 +14,8 @@ namespace Components {
 
 EPS_Sim ::EPS_Sim(const char* const compName, F32 delta) : EPS_SimComponentBase(compName) {
     this->_delta = delta;
+    _opMode = Components::OpMode::OFF;
+    _batteryLevel = 100;
 }
 
 EPS_Sim ::~EPS_Sim() {}
@@ -27,10 +29,10 @@ void EPS_Sim ::getBattery_handler(FwIndexType portNum, F32 &batteryLevel) {
 }
 
 void EPS_Sim ::run_handler(FwIndexType portNum, U32 context) {
-    if (_opMode) {
-        _batteryLevel += _opMode * _delta;
+    if (_opMode != Components::OpMode::OFF) {
+        _batteryLevel += _opMode * _delta; // _opMode is negative while in draining mode, and positive while in charging mode
 
-        if (_batteryLevel < 0) _batteryLevel = 0;
+        if (_batteryLevel < 0) _batteryLevel = 0; // These lines just cap the battery level at realistic levels
         else if (_batteryLevel > 100) _batteryLevel = 100;
     }
 }
